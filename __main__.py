7ab77d761779be4b1fd5ed8e45314c20a57cc647
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-21 08:14:44 -0600

    Basic image and play button

--- __main__.py
+++ __main__.py
@@ -3,12 +3,20 @@ from PIL import Image,ImageTk
 import tkinter as tk
 #window setup code
 win = tk.Tk()
-loop = True
-canvas= Canvas(win, width= 600, height= 400)
-canvas.pack()
-img= ImageTk.PhotoImage(Image.open("fabio.png"))  
-img= ImageTk.PhotoImage(Image.open("loogey.png"))       
-        
-        
 
+canvas = Canvas(win, width = 1500, height = 900)
+canvas.pack()
+img2 = Image.open("loogey.png")
+img2 = img2.resize((1500, 900)) 
+img2 = ImageTk.PhotoImage(img2)
+canvas.create_image(10,10,anchor=NW,image=img2)
+
+button = tk.Button(
+    text="Play",
+    width=50,
+    height=10,
+    bg="brown",
+    fg="black",
+)
+button.pack()
 win.mainloop()
