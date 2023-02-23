1b81d9d79276bbbe84bd082019845e6b5e0c3801
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-23 08:17:20 -0600

    Changed menu image method

--- __main__.py
+++ __main__.py
@@ -3,13 +3,8 @@ from PIL import Image,ImageTk
 import tkinter as tk
 #window setup code
 win = tk.Tk()
-canvas = Canvas(win, width = 0, height = 0)
-canvas.pack(fill=BOTH, expand=True)
-img2 = Image.open("loogey.png")
-img2 = img2.resize((1500, 900)) 
-img2 = ImageTk.PhotoImage(img2)
-canvas.create_image(10,10,anchor=NW,image=img2)
-
+mainMenuImg = tk.PhotoImage(file = 'game_menu.png')
+menuBackGround = Label(win, i = mainMenuImg)
 button = tk.Button(
     text="Play",
     width=60,
@@ -18,4 +13,5 @@ button = tk.Button(
     fg="black",
 )
 button.pack()
+menuBackGround.pack()
 win.mainloop()
