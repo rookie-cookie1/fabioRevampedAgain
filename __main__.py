ed18921929ef404e027ad336adc080506000f341
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-23 10:36:20 -0600

    INCOMPLETE CODE

--- __main__.py
+++ __main__.py
@@ -1,8 +1,21 @@
 from tkinter import * 
 from PIL import Image,ImageTk
 import tkinter as tk
+
+
+menuButtons = ['Play Game', 'Settings', 'Options']
+for index in len(menuButtons):
+    tk.Button(
+        text = menuButtons[index]
+        width = 60
+        height = 3
+        
+    ).pack
+
 #window setup code
 win = tk.Tk()
+win.geometry("1817x900")
+win.resizable(False, False)
 mainMenuImg = tk.PhotoImage(file = 'game_menu.png')
 menuBackGround = Label(win, i = mainMenuImg)
 button = tk.Button(
@@ -12,6 +25,8 @@ button = tk.Button(
     bg="brown",
     fg="black",
 )
-button.pack()
 menuBackGround.pack()
+
+button.pack()
+button.place(x = 720, y = 500)
 win.mainloop()
