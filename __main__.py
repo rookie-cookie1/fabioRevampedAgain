f1a1240e29495bcbad44b9b4d52f64976a528a43
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-27 08:17:04 -0600

    gew;iojwlgpo;jkl;jkl

--- __main__.py
+++ __main__.py
@@ -7,12 +7,14 @@ win.geometry("1817x860")
 win.resizable(False, False)
 
 def MainMenu():
+    #The main menu layout is contained in this function
     global win
     global button
     menuBackGround = Label(win, i = mainMenuImg)
     menuBackGround.pack()
     YposSpace = 60
     adjustedY = 540
+    #Creates as menu buttons based off of the menuButtons list
     for item in menuButtons:
         button = tk.Button(
             win,
