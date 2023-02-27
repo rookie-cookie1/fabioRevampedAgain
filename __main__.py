36e550c4cb1f062af42cf1b56cdae891e21b9fd7
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-27 10:36:22 -0600

    Changed the button layout method

--- __main__.py
+++ __main__.py
@@ -5,8 +5,10 @@ import tkinter as tk
 win = tk.Tk()
 win.geometry("1817x860")
 win.resizable(False, False)
-
+def mainMenuFunction(bName):
+    print(bName)
 def MainMenu():
+    #Calling this function brings up the visual main menu
     #The main menu layout is contained in this function
     global win
     global button
@@ -14,23 +16,18 @@ def MainMenu():
     menuBackGround.pack()
     YposSpace = 60
     adjustedY = 540
-    #Creates as menu buttons based off of the menuButtons list
-    for item in menuButtons:
-        button = tk.Button(
-            win,
-            text = item,
-            width=60,
-            height=3,
-            bg="brown",
-            fg="black",
-        )
-        button.pack()
-        adjustedY =  adjustedY - YposSpace
-        print(str(adjustedY))
-        button.place(x = 720, y = adjustedY)
+    playGame =  tk.Button(
+        win,
+        text = menuButtons[i],
+        width = 60,
+        height = 3,
+        bg = "brown",
+        fg = "black",
+        command=lambda: mainMenuFunction(str())
+    )
+
 
 mainMenuImg = tk.PhotoImage(file = 'game_menu.png')
-menuButtons = ['Exit Game', 'Options', 'Play Game']
 MainMenu()
 
 
