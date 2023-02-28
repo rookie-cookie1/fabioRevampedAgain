367af0a5d49ac243017867a0716b4d6a0380204a
Riley Whitehead <ender.storm1234@gmail.com>
2023-02-28 08:17:16 -0600

    Added character sprites
    Co-authored-by: Sussysam69 <Sussysam69@users.noreply.github.com>

--- __main__.py
+++ __main__.py
@@ -1,10 +1,35 @@
 from tkinter import * 
 from PIL import Image,ImageTk
 import tkinter as tk
+import keyboard
 #window setup code
 win = tk.Tk()
 win.geometry("1817x860")
 win.resizable(False, False)
+def gameFunc():
+    theCanvas = tk.Canvas(
+        win, 
+        bg = 'white', 
+        height = 860, 
+        width = 1817
+        )
+    coord = 10, 10, 300, 300
+    arc = theCanvas.create_arc(coord, start=0, extent=150, fill="red")
+    arv2 = theCanvas.create_arc(coord, start=150, extent=215, fill="green")
+    theCanvas.pack()
+    while True:
+        try:
+            if keyboard.is_pressed("w"):
+                print('im cumminggggg')
+            if keyboard.is_pressed("a"):
+                print('hehehe')
+            if keyboard.is_pressed("s"):
+                print('NO')
+            if keyboard.is_pressed("d"):
+                break
+        except:
+            break
+        
 def mainMenuFunction(bName):
     if bName == 2:
         exit()
@@ -12,11 +37,10 @@ def mainMenuFunction(bName):
         pass
         print('Haha')
     elif bName == 0:
-        pass
+        gameFunc()
         print('Bruh')
     else:
         print('ERROR: NO MATCHING BUTTON FUNCTION - PLEASE DEFINE IN "mainMenuFunction()"')
-
 def MainMenu():
     #Calling this function brings up the visual main menu
     #The main menu layout is contained in this function
@@ -59,8 +83,6 @@ def MainMenu():
 
 
 mainMenuImg = tk.PhotoImage(file = 'game_menu.png')
-mainMenuBDic = {}
-mainMenuBName = ['Play Game', 'Options', 'Exit']
 MainMenu()
 
 
